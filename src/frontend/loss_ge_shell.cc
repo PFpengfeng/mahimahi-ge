7fd28d0e15d4f619223680968a77b2a3de702a96
PFpengfeng <pengfeng.peng@alibaba-inc.com>
2023-07-04 16:01:07 +0800

    file save

--- src/frontend/loss_ge_shell.cc
+++ src/frontend/loss_ge_shell.cc
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <string>
+#include <sstream>
 #include <iostream>
 
 #include <getopt.h>
@@ -29,35 +30,45 @@ int main( int argc, char *argv[] )
 
         check_requirements( argc, argv );
 
-        if ( argc < 3 ) {
+        if ( argc < 2 ) {
             usage( argv[ 0 ] );
         }
 
-        const double loss_rate = myatof( argv[ 2 ] );
-        if ( (0 <= loss_rate) and (loss_rate <= 1) ) {
-            /* do nothing */
-        } else {
-            cerr << "Error: loss rate must be between 0 and 1." << endl;
+        stringstream argss( argv[ 2 ] );
+        string token;
+        // const double g2b_r, b2g_r, lr_g, lr_b;
+        double rate_log[4];
+        memset( rate_log, 0, sizeof(rate_log) );
+        int rate_num = 0;
+
+        while( getline(argss, token, ',') ){
+            if( rate_num >= 4){
+                cerr << "Error: only input 4 ge params." << endl;
+                usage( argv[ 0 ] );
+            }
+            rate_log[rate_num] = myatof(token);
+            ++rate_num;
+        }
+        if ( ( rate_num != 4) ){
+            cerr << "Error: must input 4 ge params." << endl;
             usage( argv[ 0 ] );
         }
-
-        double uplink_loss = 0, downlink_loss = 0;
-
-        const string link = argv[ 1 ];
-        if ( link == "uplink" ) {
-            uplink_loss = loss_rate;
-        } else if ( link == "downlink" ) {
-            downlink_loss = loss_rate;
-        } else {
-            usage( argv[ 0 ] );
+        for(int i = 0; i < 3; ++i){
+            if( (0 <= rate_log[i] ) and ( rate_log[i] <= 1) ){
+                /* do nothing */
+            }
+            else{
+                cerr << "Error: loss rate must be between 0 and 1." << endl;
+                usage( argv[ 0 ] );
+            }
         }
 
         vector<string> command;
 
-        if ( argc == 3 ) {
+        if ( argc == 2 ) {
             command.push_back( shell_path() );
         } else {
-            for ( int i = 3; i < argc; i++ ) {
+            for ( int i = 2; i < argc; i++ ) {
                 command.push_back( argv[ i ] );
             }
         }
@@ -65,18 +76,14 @@ int main( int argc, char *argv[] )
         PacketShell<IIDLoss> loss_app( "loss", user_environment, passthrough_until_signal );
 
         string shell_prefix = "[loss ";
-        if ( link == "uplink" ) {
-            shell_prefix += "up=";
-        } else {
-            shell_prefix += "down=";
-        }
-        shell_prefix += argv[ 2 ];
+        shell_prefix += "ge_params=";
+        shell_prefix += argv[ 1 ];
         shell_prefix += "] ";
 
         loss_app.start_uplink( shell_prefix,
                                command,
                                uplink_loss );
-        loss_app.start_downlink( downlink_loss );
+        loss_app.start_downlink( 0 );
         return loss_app.wait_for_exit();
     } catch ( const exception & e ) {
         print_exception( e );
